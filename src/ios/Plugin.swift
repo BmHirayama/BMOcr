bc53c3b8b38c1ffafa0dc992ce47d07d7b1ce6ca
BmH <151484351+BmHirayama@users.noreply.github.com>
2024-05-31 01:42:08 +0900

    Add files via upload

---
+++ src/ios/Plugin.swift
@@ -0,0 +1,80 @@
+//
+//  Plugin.swift
+//  CardReaderSample
+//
+//  Created by 平山 裕也 on 2023/12/04.
+//
+
+import Foundation
+import UIKit
+
+@objc(Plugin)
+class Plugin : CDVPlugin, OnPluginManagerDelegate {
+    
+    // MARK: - Member
+    private var mPluginManager: PluginManager? = nil
+    private var mOcrCallbackId: String? = nil
+    
+    // MARK: - OnPluginManagerDelegate
+    func onResult(sender: PluginManager, code: Int, result: OcrResultInfo?) {
+        stopOcr()
+        if (mOcrCallbackId != nil) {
+            if (code == PluginManager.CODE_SUCCESS) {
+                let inType = result?.mCardType ?? 0
+                let inName = result?.mName ?? ""
+                let inGender = result?.mGender ?? ""
+                let inAddress = result?.mAddress ?? ""
+                let inBirthdate = result?.mBirthdate ?? ""
+                let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: ["errorCode": 0, "type": inType, "name": inName, "gender": inGender, "address": inAddress, "birthdate": inBirthdate ])
+                pluginResult?.keepCallback = true
+                commandDelegate.send(pluginResult, callbackId: mOcrCallbackId)
+            } else if (code == PluginManager.CODE_AUTHROIZE) {
+                let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: ["errorCode": -2] )
+                pluginResult?.keepCallback = true
+                commandDelegate.send(pluginResult, callbackId: mOcrCallbackId)
+            } else {
+                let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: ["errorCode": -1] )
+                pluginResult?.keepCallback = true
+                commandDelegate.send(pluginResult, callbackId: mOcrCallbackId)
+            }
+        }
+    }
+        
+    // MARK: - Accesser
+    @objc(startOCR:)
+    func startOCR(command: CDVInvokedUrlCommand) {
+        mOcrCallbackId = command.callbackId
+        startOcr()
+    }
+    
+    // MARK: - Function
+    private func getPluginManager() -> PluginManager {
+        if (mPluginManager == nil) {
+            mPluginManager = PluginManager()
+        }
+        return mPluginManager!
+    }
+    
+    private func topMostController() -> UIViewController? {
+        return UIApplication.shared.keyWindow?.rootViewController?.presentedViewController
+    }
+    
+    private func startOcr() {
+        let viewController = topMostController()
+        if (viewController == nil) {
+            if (mOcrCallbackId != nil) {
+                let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: ["errorCode": -1] )
+                pluginResult?.keepCallback = true
+                commandDelegate.send(pluginResult, callbackId: mOcrCallbackId)
+            }
+            return
+        }
+        getPluginManager().start(parent: viewController!, delegate: self)
+    }
+    
+    private func stopOcr() {
+        getPluginManager().stop()
+        mPluginManager = nil
+    }
+
+}
