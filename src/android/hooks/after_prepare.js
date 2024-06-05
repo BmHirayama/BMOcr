46dea95650dd61e4741bfcf9c7b91553538fea43
BmHirayama <151484351+BmHirayama@users.noreply.github.com>
2024-06-05 10:36:30 +0900

    change

--- src/android/hooks/after_prepare.js
+++ src/android/hooks/after_prepare.js
@@ -53,51 +53,7 @@ module.exports = function (context) {
     }
 
     checkForDuplicatePermissions(plugin, androidManifest);
-
-    setGradleProperties();
 };
-
-function setGradleProperties() {
-    const PLUGIN_NAME = "cordova-plugin-androidx";
-    const jvmargs = "org.gradle.jvmargs=-Xmx2048m -Dfile.encoding=UTF-8";
-    const enableAndroidX = "android.useAndroidX=true";
-    const setJetifier = "android.enableJetifier=true";
-    const style = "kotlin.code.style=official";
-    const nonTransitiveRClass = "android.nonTransitiveRClass=true";
-    const gradlePropertiesPath = "./platforms/android/gradle.properties";
-
-    let gradleProperties = fs.readFileSync(gradlePropertiesPath);
-    if (gradleProperties) {
-        let updatedGradleProperties = false;
-        gradleProperties = gradleProperties.toString();
-        if (!gradleProperties.match(jvmargs)) {
-            gradleProperties += "\n" + jvmargs;
-            updatedGradleProperties = true;
-        }
-        if (!gradleProperties.match(enableAndroidX)) {
-            gradleProperties += "\n" + enableAndroidX;
-            updatedGradleProperties = true;
-        }
-        if (!gradleProperties.match(setJetifier)) {
-            gradleProperties += "\n" + setJetifier;
-            updatedGradleProperties = true;
-        }
-        if (!gradleProperties.match(style)) {
-            gradleProperties += "\n" + style;
-            updatedGradleProperties = true;
-        }
-        if (!gradleProperties.match(nonTransitiveRClass)) {
-            gradleProperties += "\n" + nonTransitiveRClass;
-            updatedGradleProperties = true;
-        }
-        if (updatedGradleProperties) {
-            fs.writeFileSync(gradlePropertiesPath, gradleProperties, 'utf8');
-        }
-    } else {
-//        log("gradle.properties file not found!")
-    }
-
-}
 
 function checkForDuplicatePermissions(plugin, androidManifest) {
     const permissionsRegex = /<uses-permission.*?android:name="(?<permission>android\.permission\..*?)".*?\/>/gm;
