b0c4c215cc3bae86db699555bd3e733aee117865
BmHirayama <151484351+BmHirayama@users.noreply.github.com>
2024-07-05 13:55:33 +0900

    delete

--- www/MyPlugin.js
+++
@@ -1,9 +0,0 @@
-var exec = require('cordova/exec');
-
-var MyPlugin = {
-    coolMethod: function(success, error) {
-        exec(success, error, "MyPlugin", "coolMethod", [arg0]);
-    }
-};
-
-module.exports = MyPlugin;
